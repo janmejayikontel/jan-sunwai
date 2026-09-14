adb4f09dd3086a0314188ec942e3d8526d796400
Sahil Kumar Rout <sahil.rout@ikontel.com>
2026-09-14 12:22:53 +0530

    fix(mobile): update default server URL to public endpoint

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -10,7 +10,7 @@
 
 import { Platform } from 'react-native';
 
-export const DEFAULT_SERVER_URL = 'http://172.18.178.78:8080';
+export const DEFAULT_SERVER_URL = 'https://694109cd430525fc-164-100-222-44.serveousercontent.com';
 
 
 export const CONFIG = {
