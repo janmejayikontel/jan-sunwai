9bf809e8f58716769487d93c001b576600399f77
Sahil Kumar Rout <sahil.rout@ikontel.com>
2026-09-23 13:02:52 +0530

    Update live server URL to active tunnel

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -10,7 +10,7 @@
 
 import { Platform } from 'react-native';
 
-export const DEFAULT_SERVER_URL = 'https://innovation-laundry-realtor-carefully.trycloudflare.com';
+export const DEFAULT_SERVER_URL = 'https://organisms-issues-pounds-horizontal.trycloudflare.com';
 
 
 export const CONFIG = {
